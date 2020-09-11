28938c4b67c3697dc3107c0b4ce74a5af0b30fc4
duongtora <nvduong216@gmail.com>
2020-09-11 15:11:27 +0700

    20200911 15:11 commit1 : first commit

---
+++ src/main/java/com/duongnv30/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.duongnv30.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/trang-chu", method = RequestMethod.GET)
+	public ModelAndView homeController() {
+		ModelAndView mav = new ModelAndView("home");
+		return mav;
+	}
+}
