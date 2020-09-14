c5f9aa598b1494fa07b8d00587ff2b754dbaf146
duongtora <nvduong216@gmail.com>
2020-09-14 11:42:27 +0700

    20200914 11:41 commit8 : add JPA to project

---
+++ src/main/java/com/duongnv30/entity/NewEntity.java
@@ -0,0 +1,65 @@
+package com.duongnv30.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "new")
+public class NewEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "title", columnDefinition = "nvarchar(255)", nullable = false)
+	private String title;
+
+	@Column(name = "thumbnail", columnDefinition = "nvarchar(255)")
+	private String thumbnail;
+
+	@Column(name = "shortdescription", columnDefinition = "nvarchar(max) default('d')", nullable = false)
+	private String shortDescription;
+
+	@Column(name = "content", columnDefinition = "nvarchar(max)", nullable = false)
+	private String content;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getThumbnail() {
+		return thumbnail;
+	}
+
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
